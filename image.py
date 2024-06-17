2057b087a3974635c3566c51b6db8d7d7055d939
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-06-17 16:44:16 -0400

    grayscaling done

--- image.py
+++ image.py
@@ -2,24 +2,41 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
+scale = ".,-~:;=!*#$@"
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+
+def get_density(val):
+    tmp = round(val / (255 / len(scale)))
+    return scale[tmp]
+# val / (255 / 12)
+
 image_path = 'daniel.jpg' 
 image = Image.open(image_path)
 
 # Convert image to a numpy
 image_array = np.array(image)
-print(image_array)
 
 height, width, channels = image_array.shape
 
+image_array = rgb2gray(image_array)
 # Loop through each pixel and invert the colors
-for i in range(height):
-    for j in range(width):
-        r, g, b = image_array[i, j]
-
+# for i in range(height):
+#     for j in range(width):
+#         r, g, b = image_array[i][j]
+#         print(r, g, b)
+        
 # Convert the numpy array back to an image
-back = Image.fromarray(image_array)
+image = Image.fromarray(image_array)
 
 # Display the image using matplotlib
-plt.imshow(back)
+plt.imshow(image)
 plt.axis('off')
 plt.show()
+
+for i in range(height):
+    row = ""
+    for j in range(width):
+        row += get_density(image_array[i][j])
+    print(row)
