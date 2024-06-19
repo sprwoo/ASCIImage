bfec4620004fa5adb37635156a4b9890cd7c51fb
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-06-19 19:36:26 -0400

    optimization updates as well as turning them into functions

--- image.py
+++ image.py
@@ -1,46 +1,46 @@
+# Imports
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-scale = "   .,-~:;=!*#$@"
+# Constants
+DENSITIES = "   .,-~:;=!*#$@"
+RESIZE = (50, 50)
+NTSC_CONST = np.array([0.2989, 0.587, 0.114])
 
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+def NTSC_formula(rgb):
+    return np.dot(rgb, NTSC_CONST.T)
 
-def get_density(val):
-    tmp = round(val / (255 / len(scale)))
-    return scale[tmp]
+def density_character(val):
+    tmp = round(val / (255 / len(DENSITIES)))
+    return DENSITIES[tmp]
 
-image_path = 'pfp.png' 
-image = Image.open(image_path)
+def convert_to_ascii(image_array):
+    for i in range(height):
+        row = ""
+        for j in range(width):
+            pixel = NTSC_formula(image_array[i][j][:3])
+            row += density_character(pixel)
+        print(row)
 
-newsize = (50, 50)
-image = image.resize(newsize)
+def show_image(image):
+    # Display the image using matplotlib
+    plt.imshow(image)
+    plt.axis('off')
+    plt.show()
 
-# Convert image to a numpy
-image_array = np.array(image)
+if __name__ == '__main__':
+    image_path = 'pfp.png' 
+    image = Image.open(image_path)
+    image = image.resize(RESIZE)
+    show_image(image)
 
-height, width, channels = image_array.shape
+    # Convert image to a numpy
+    image_array = np.array(image)
+    height, width, channels = image_array.shape
+    convert_to_ascii(image_array)
 
-image_array = rgb2gray(image_array)
-# Loop through each pixel and invert the colors
-# for i in range(height):
-#     for j in range(width):
-#         r, g, b = image_array[i][j]
-#         print(r, g, b)
-        
-# Convert the numpy array back to an image
-image = Image.fromarray(image_array)
+    image.close()
 
-# Display the image using matplotlib
-plt.imshow(image)
-plt.axis('off')
-plt.show()
 
-for i in range(height):
-    row = ""
-    for j in range(width):
-        row += get_density(image_array[i][j])
-    print(row)
 
-image.close()
