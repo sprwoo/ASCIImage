c53c6390488af46b8d63264477ba8a7bec194333
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-06-19 20:00:09 -0400

    tkinter

--- image.py
+++ image.py
@@ -2,10 +2,12 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import messagebox
 
 # Constants
-DENSITIES = "    .'`^\",:;Il!i><~+_-?][}{1)(|\\/#MW&8%B@$"
-RESIZE = (50, 50)
+DENSITIES = "  .'`^\",:;Il!i><~+_-?][}{1)(|\\/#MW&8%B@$"
+RESIZE = (30, 30)
 NTSC_CONST = np.array([0.2989, 0.587, 0.114])
 
 def NTSC_formula(rgb):
@@ -16,12 +18,13 @@ def density_character(val):
     return DENSITIES[tmp]
 
 def convert_to_ascii(image_array):
+    row = ""
     for i in range(height):
-        row = ""
         for j in range(width):
             pixel = NTSC_formula(image_array[i][j][:3])
             row += density_character(pixel)
-        print(row)
+        row += "\n"
+    return row
 
 def show_image(image):
     # Display the image using matplotlib
@@ -30,15 +33,25 @@ def show_image(image):
     plt.show()
 
 if __name__ == '__main__':
-    image_path = 'daniel.jpg' 
+    image_path = 'pfp.png' 
     image = Image.open(image_path)
     image = image.resize(RESIZE)
-    show_image(image)
+    # show_image(image)
 
     # Convert image to a numpy
     image_array = np.array(image)
     height, width, channels = image_array.shape
-    convert_to_ascii(image_array)
+    ascii_image = convert_to_ascii(image_array)
+
+    root = tk.Tk()
+    root.geometry("500x700")
+    root.title("Window")
+
+    window = tk.Label(root, text=ascii_image, anchor='w', justify='left', font=('Courier New', 14), highlightthickness=0, borderwidth=0, pady=0)
+    window.pack()
+
+    root.mainloop()
 
     image.close()
+
 
