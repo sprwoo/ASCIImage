44d2b8d96508935a36d6bbe333adca036f797257
Daniel Yang <91350928+sprwoo@users.noreply.github.com>
2024-08-16 15:27:45 -0400

    styling

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -2,33 +2,35 @@ import React from "react";
 import { useState } from "react";
 
 const App = () => {
-    const [selectedImage, setSelectedImage] = useState(null);
+    const [image, setImage] = useState(null);
     
     const handleImage = (e) => {
         const file = e.target.files[0];
         if (file) {
             const reader = new FileReader();
             reader.onloadend = () => {
-                setSelectedImage(reader.result);
+                setImage(reader.result);
             }
             reader.readAsDataURL(file);
         }
     }
 
 
-    
     return (
-        <div id="imageUpload">
-            <input
-                type="file"
-                accept="image/*"
-                onChange={handleImage}
-            />
-            {selectedImage && (
-                <div>
-                    <img src={selectedImage} alt="Selected" style={{ width: '300px', height: 'auto' }} />
-                </div>
-            )}
+        <div class="main-container">
+            <h1>ASCIImage</h1>
+            <div id="imageUpload">
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImage}
+                />
+                {image && (
+                    <div>
+                        <img src={image} alt="Selected" style={{ width: '300px', height: 'auto' }} />
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
